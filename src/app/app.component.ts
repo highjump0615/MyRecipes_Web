99a2acae4a28259c5790c9943564f5c881f60064
highjump <lixiang901112@163.com>
2018-11-09 21:13:32 +0800

    Added UI for onboard page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,6 @@ import {Component, NgZone} from '@angular/core';
 export class AppComponent {
   title = 'app';
 
-  owlCarouselOptions: Object; // 宣告 Owl Carousel 的 Options
-
-  constructor(private _zone: NgZone) {
-    this.owlCarouselOptions = { // 設定 Owl Carousel Options
-      items: 5,
-      dots: true,
-      navigation: true,
-      nav: true,
-      autoplay: true,
-      autoplayTimeout: 3000,
-      autoplayHoverPause: false,
-      loop: true,
-      onTranslate: (e) => _zone.run(() => console.log('is translated, the page index is: ' + e.page.index)),
-    };
+  constructor() {
   }
 }
--- src/app/page/onboard/onboard.component.ts
+++ src/app/page/onboard/onboard.component.ts
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnboardComponent implements OnInit {
 
-  constructor() { }
+  owlCarouselOptions: Object; // 宣告 Owl Carousel 的 Options
+
+  constructor() {
+    this.owlCarouselOptions = { // 設定 Owl Carousel Options
+      items: 1,
+      navigation: true,
+      onTranslate: (e) => {
+        console.log('is translated, the page index is: ' + e.page.index);
+      }
+    };
+  }
 
   ngOnInit() {
   }
